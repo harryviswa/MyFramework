7dc49d6cef3143554071153c3c8c48505eefe7c9
Hariharan Viswanathan <harry.viswa@gmail.com>
2023-07-30 16:37:21 +0400

    Harry | Minor updates

--- src/main/java/com/apps/trail/pages/EmiratesBooking.java
+++ src/main/java/com/apps/trail/pages/EmiratesBooking.java
@@ -1,5 +1,9 @@
 package com.apps.trail.pages;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedHashMap;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -32,19 +36,23 @@ public class EmiratesBooking  extends CommonTestActions {
 		optionalClick("//button[text()='Accept']", "Accept Terms");
 	}
 	
-	public void enterBookingDetails(String from,String to,String fromdate,String returnDate,int adults,int child,int infants) {
-		typeAndSelect(input_departure, from, "select value"); 
-		typeAndSelect(input_arrival,to, "select value"); 
-		click("//td[@data-string='"+fromdate+"']/button","Select from Date");
+	public void enterBookingDetails(LinkedHashMap<String,String> data) {
+		typeAndSelect(input_departure, data.get("From"), "select value"); 
+		typeAndSelect(input_arrival,data.get("To"), "select value"); 
+		Calendar dt=Calendar.getInstance();
+		dt.add(Calendar.MONTH, 1);
+		int dti=dt.get(Calendar.MONTH);
+		String returnDate=data.get("ReturnDate").replace("X", ""+dti);
+		click("//td[@data-string='"+data.get("FromDate").replace("X", ""+dti)+"']/button","Select from Date");
 		if(returnDate.equals("") && isWebElementPresent(lbl_ReturningText,2))
 			click(chkbox_oneWay, "OneWay");
 		else
 			click("//td[@data-string='"+returnDate+"']/button","Select return Date");
 		
 		String passenger="//div[@data-channel='passenger' and @data-type='XXX']/button[@aria-disabled='false']";
-		for(int i=1;i<adults;i++) click(passenger.replace("XXX","adults"),"Adults");
-		for(int i=0;i<child;i++) click(passenger.replace("XXX","children"),"Children");
-		for(int i=0;i<infants;i++) click(passenger.replace("XXX","infants"),"Infants");
+		for(int i=1;i<Integer.parseInt(data.get("Adults"));i++) click(passenger.replace("XXX","adults"),"Adults");
+		for(int i=0;i<Integer.parseInt(data.get("Child"));i++) click(passenger.replace("XXX","children"),"Children");
+		for(int i=0;i<Integer.parseInt(data.get("Infants"));i++) click(passenger.replace("XXX","infants"),"Infants");
 
 		click(btn_searchFlight,"Search button");
 		assertStep(isWebElementPresent("//h1[contains(text(),'Make a booking')]",60), "Search Result Validation");
